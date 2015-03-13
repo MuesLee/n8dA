0f7ab8cf311da0bffce7493a7f4e40d2d2703ed0
Marvin Bruns <android.bruns@googlemail.com>
2015-03-13 12:33:48 +0100

    umzug

--- de.kvwl.n8dA.robotwars/src/main/java/de/kvwl/n8dA/robotwars/visualization/AnimationPosition.java
+++ de.kvwl.n8dA.robotwars/src/main/java/de/kvwl/n8dA/robotwars/visualization/AnimationPosition.java
@@ -1,37 +1,43 @@
 package de.kvwl.n8dA.robotwars.visualization;
 
-public class AnimationPosition {
+// Welche Animation wird für welchen Roboter ausgeführt
+public class AnimationPosition
+{
 
-	
 	private String animationID;
-	
+
 	private RobotPosition position;
 
-	public AnimationPosition(String animationID, RobotPosition position) {
+	public AnimationPosition(String animationID, RobotPosition position)
+	{
 		super();
 		this.animationID = animationID;
 		this.position = position;
 	}
 
-	public String getAnimationID() {
+	public String getAnimationID()
+	{
 		return animationID;
 	}
 
-	public void setAnimationID(String animationID) {
+	public void setAnimationID(String animationID)
+	{
 		this.animationID = animationID;
 	}
-	
+
 	/**
 	 * Get the position of the Robot which starts the animation
+	 * 
 	 * @return
 	 */
-	public RobotPosition getPosition() {
+	public RobotPosition getPosition()
+	{
 		return position;
 	}
 
-	public void setPosition(RobotPosition position) {
+	public void setPosition(RobotPosition position)
+	{
 		this.position = position;
 	}
-	
-	
+
 }
