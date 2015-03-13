8796a7c5655f9448a8067b3ff52392929c364869
MuesLee <stimo@gmx.net>
2015-03-13 11:12:06 +0100

    Refactoring Moved Visualization of the Battle to the Server Project, for it will run on the same machine.

--- de.kvwl.n8dA.robotwars/src/main/java/de/kvwl/n8dA/robotwars/controller/BattleController.java
+++ de.kvwl.n8dA.robotwars/src/main/java/de/kvwl/n8dA/robotwars/controller/BattleController.java
@@ -32,6 +32,8 @@ public class BattleController {
 			throw new RobotsArentRdyToFightException();
 		}
 		
+		
+		
 	}
 
 
---
+++ de.kvwl.n8dA.robotwars/src/main/java/de/kvwl/n8dA/robotwars/visualization/CinematicVisualizer.java
@@ -0,0 +1,5 @@
+package de.kvwl.n8dA.robotwars.visualization;
+
+public class CinematicVisualizer implements CinematicHandler {
+
+}
