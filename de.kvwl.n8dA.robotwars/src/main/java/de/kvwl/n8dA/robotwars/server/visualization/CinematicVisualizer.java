b20e7df76b01ebd8557c6070d2e2f4162cbde2de
Marvin Bruns <android.bruns@googlemail.com>
2015-03-16 09:06:01 +0100

    ...

--- de.kvwl.n8dA.robotwars/src/main/java/de/kvwl/n8dA/robotwars/server/visualization/CinematicVisualizer.java
+++ de.kvwl.n8dA.robotwars/src/main/java/de/kvwl/n8dA/robotwars/server/visualization/CinematicVisualizer.java
@@ -4,13 +4,15 @@ import java.util.List;
 
 import de.kvwl.n8dA.robotwars.commons.game.entities.Robot;
 
-public interface CinematicVisualizer {
+//TODO GUI umsetzen
+public interface CinematicVisualizer
+{
 
 	/**
 	 * Es haben sich zwei Robos angemeldet. Der Kampf beginnt
 	 */
 	public void battleIsAboutToStart();
-	
+
 	/**
 	 * Ein Roboter hat sich auf dem Server angemeldet und tritt der Arena bei
 	 * 
@@ -18,26 +20,22 @@ public interface CinematicVisualizer {
 	 * @param position
 	 */
 	public void robotHasEnteredTheArena(Robot robot, RobotPosition position);
-	
+
 	/**
 	 * Für beide Robos stehen Aktionen bereit. Die Runde beginnt.
 	 */
 	public void roundIsAboutToStart();
-	
+
 	/**
 	 * Nach 70% der Animationszeit der ersten Animation, soll die zweite Animation starten
 	 */
 	public void playAnimationForRobotsWithDelayAfterFirst(List<AnimationPosition> animations);
-	
+
 	/**
 	 * Beide Animationen sollen parallel starten
 	 * 
 	 * @param animations
 	 */
 	public void playAnimationForRobotsSimultaneously(List<AnimationPosition> animations);
-	
-	
-	
-	
-	
+
 }
