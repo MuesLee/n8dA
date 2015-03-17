8f120e025f1d01b4b515637f6d27501ac4a26f30
MuesLee <stimo@gmx.net>
2015-03-17 09:33:52 +0100

    Cleaning

--- de.kvwl.n8dA.robotwars.commons/src/main/java/de/kvwl/n8dA/robotwars/commons/game/util/GameStateType.java
+++ de.kvwl.n8dA.robotwars.commons/src/main/java/de/kvwl/n8dA/robotwars/commons/game/util/GameStateType.java
@@ -5,7 +5,6 @@ public enum GameStateType {
 	VICTORY_LEFT, VICTORY_RIGHT, DRAW, BATTLE_IS_ACTIVE, WAITING_FOR_PLAYER_INPUT, GAME_HAS_BEGUN, GAME_HASNT_BEGUN;
 
 	public static String getNotificationName() {
-		// TODO Auto-generated method stub
 		return "GAME_STATE_INFO";
 	}
 
