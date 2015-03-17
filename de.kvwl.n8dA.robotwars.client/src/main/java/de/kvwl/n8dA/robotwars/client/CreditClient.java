b91a5d8327d98bd785d2cf30008cf4e6619124d8
Marvin Bruns <android.bruns@googlemail.com>
2015-03-17 11:17:30 +0100

    todos

---
+++ de.kvwl.n8dA.robotwars.client/src/main/java/de/kvwl/n8dA/robotwars/client/CreditClient.java
@@ -0,0 +1,12 @@
+package de.kvwl.n8dA.robotwars.client;
+
+//TODO Timo: Implementierung
+/**
+ * 
+ * Verbindung zu unserem Punkteserver
+ *
+ */
+public interface CreditClient
+{
+
+}
--- de.kvwl.n8dA.robotwars/src/main/java/de/kvwl/n8dA/robotwars/server/input/DataLoaderFileSystemImpl.java
+++ de.kvwl.n8dA.robotwars/src/main/java/de/kvwl/n8dA/robotwars/server/input/DataLoaderFileSystemImpl.java
@@ -7,32 +7,38 @@ import de.kvwl.n8dA.robotwars.commons.game.actions.Defense;
 import de.kvwl.n8dA.robotwars.commons.game.entities.Robot;
 import de.kvwl.n8dA.robotwars.commons.gui.Animation;
 
-public class DataLoaderFileSystemImpl implements DataLoader {
+public class DataLoaderFileSystemImpl implements DataLoader
+{
 
-	//TODO: CODE ME, BRUNO :)
-	
+	//TODO Marvin: Animation loader
+
 	@Override
-	public List<Animation> loadAnimationsForRobots() {
+	public List<Animation> loadAnimationsForRobots()
+	{
 		return null;
 	}
 
 	@Override
-	public List<Animation> loadAnimationsForRobotActions() {
+	public List<Animation> loadAnimationsForRobotActions()
+	{
 		return null;
 	}
 
 	@Override
-	public List<Robot> loadRobots() {
+	public List<Robot> loadRobots()
+	{
 		return null;
 	}
 
 	@Override
-	public List<Attack> loadRobotAttacks() {
+	public List<Attack> loadRobotAttacks()
+	{
 		return null;
 	}
 
 	@Override
-	public List<Defense> loadRobotDefends() {
+	public List<Defense> loadRobotDefends()
+	{
 		return null;
 	}
 
