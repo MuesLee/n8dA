18dd7e851800b1bccfcdf103d3bf32a1ca282de3
MuesLee <stimo@gmx.net>
2015-03-17 09:23:50 +0100

    added name + configurationPOintCosts for entity

--- de.kvwl.n8dA.robotwars.commons/src/main/java/de/kvwl/n8dA/robotwars/commons/game/entities/Entity.java
+++ de.kvwl.n8dA.robotwars.commons/src/main/java/de/kvwl/n8dA/robotwars/commons/game/entities/Entity.java
@@ -5,12 +5,13 @@ import java.io.Serializable;
 public abstract class Entity implements Serializable
 {
 
-	//TODO Namen
-	//TODO Kosten
 
 	private static final long serialVersionUID = 1L;
 	protected long id;
-
+	protected String name; 
+	protected int configurationPointCosts;
+	
+	
 	public long getId()
 	{
 		return id;
@@ -19,5 +20,21 @@ public abstract class Entity implements Serializable
 	public void setId(long id)
 	{
 		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getConfigurationPointCosts() {
+		return configurationPointCosts;
+	}
+
+	public void setConfigurationPointCosts(int configurationPointCosts) {
+		this.configurationPointCosts = configurationPointCosts;
 	}
 }
