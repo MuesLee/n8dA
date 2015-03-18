b44efb44fe9724a11c1e9f57ee45f126c692f544
Marvin Bruns <android.bruns@googlemail.com>
2015-03-18 15:05:55 +0100

    removeable

--- de.kvwl.n8dA.robotwars.commons/src/main/java/de/kvwl/n8dA/robotwars/commons/game/items/RoboItem.java
+++ de.kvwl.n8dA.robotwars.commons/src/main/java/de/kvwl/n8dA/robotwars/commons/game/items/RoboItem.java
@@ -2,9 +2,22 @@ package de.kvwl.n8dA.robotwars.commons.game.items;
 
 import de.kvwl.n8dA.robotwars.commons.game.entities.Entity;
 
-public abstract class RoboItem extends Entity implements RoboModificator{
+public abstract class RoboItem extends Entity implements RoboModificator
+{
 
 	private static final long serialVersionUID = 1L;
 
-	
+	//Wenn das Item zur Grundausrüstung gehört solle dies zu false gesetzt werden
+	private boolean removeable = true;
+
+	public boolean isRemoveable()
+	{
+		return removeable;
+	}
+
+	public void setRemoveable(boolean removeable)
+	{
+		this.removeable = removeable;
+	}
+
 }
