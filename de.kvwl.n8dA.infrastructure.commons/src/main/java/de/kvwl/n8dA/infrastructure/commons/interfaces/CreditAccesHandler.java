8c344400fe9612176c1ffc48317c475f9c8f47bb
MuesLee <stimo@gmx.net>
2015-03-18 13:57:23 +0100

    RewardServer + Client funktionieren

---
+++ de.kvwl.n8dA.infrastructure.commons/src/main/java/de/kvwl/n8dA/infrastructure/commons/interfaces/CreditAccesHandler.java
@@ -0,0 +1,13 @@
+package de.kvwl.n8dA.infrastructure.commons.interfaces;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+import de.kvwl.n8dA.infrastructure.commons.exception.NoSuchPersonException;
+
+public interface CreditAccesHandler extends Remote{
+	
+	public int getConfigurationPointsForPerson(String name)
+			throws NoSuchPersonException, RemoteException;
+
+}
--- de.kvwl.n8da.infrastructure.rewards.client/src/main/java/de/kvwl/n8da/infrastructure/rewards/client/CreditAccessClient.java
+++ de.kvwl.n8da.infrastructure.rewards.client/src/main/java/de/kvwl/n8da/infrastructure/rewards/client/CreditAccessClient.java
@@ -1,28 +1,59 @@
 package de.kvwl.n8da.infrastructure.rewards.client;
 
+import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.util.UUID;
+
+import org.apache.log4j.BasicConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import de.kvwl.n8dA.infrastructure.commons.exception.NoSuchPersonException;
+import de.kvwl.n8dA.infrastructure.commons.interfaces.CreditAccesHandler;
 import de.kvwl.n8dA.infrastructure.commons.interfaces.CreditAccess;
+import de.kvwl.n8dA.infrastructure.commons.util.NetworkUtils;
 
 public class CreditAccessClient implements CreditAccess {
 
-	
-	
+	private static final Logger LOG = LoggerFactory
+			.getLogger(CreditAccessClient.class);
+
+	private static final String url = "//"
+			+ NetworkUtils.REWARD_SERVER_HOST_IP_ADDRESS + "/"
+			+ NetworkUtils.REWARD_SERVER_NAME;
+	private CreditAccesHandler server;
+	private UUID uuid;
+
 	public CreditAccessClient() {
-		
-		
+		BasicConfigurator.configure();
+		this.uuid = UUID.randomUUID();
 	}
-	
+
 	public void initConnectionToServer() throws RemoteException {
-		// TODO Auto-generated method stub
-		
+		try {
+			server = (CreditAccesHandler) Naming.lookup(url);
+			LOG.info("Client: " + uuid + " connected to Server");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
 	}
 
 	public int getConfigurationPointsForPerson(String name)
 			throws NoSuchPersonException, RemoteException {
-		// TODO Auto-generated method stub
-		return 0;
+		return server.getConfigurationPointsForPerson(name);
+	}
+
+	public static void main(String[] args) {
+		CreditAccessClient client = new CreditAccessClient();
+		try {
+			client.initConnectionToServer();
+			client.getConfigurationPointsForPerson("Derp");
+		} catch (RemoteException e) {
+			LOG.error("Remote Error", e);
+		} catch (NoSuchPersonException e) {
+			LOG.error("Unbekannte Person", e);
+		}
 	}
 
 }
