8171374b65425cd5e495f2418ecfc595dda6875c
brunothg <android.bruns@googlemail.com>
2015-03-18 17:18:55 +0100

    user robots

--- de.kvwl.n8dA.robotwars/src/main/java/de/kvwl/n8dA/robotwars/server/input/DataLoader.java
+++ de.kvwl.n8dA.robotwars/src/main/java/de/kvwl/n8dA/robotwars/server/input/DataLoader.java
@@ -8,12 +8,17 @@ import de.kvwl.n8dA.robotwars.commons.game.entities.Robot;
 import de.kvwl.n8dA.robotwars.commons.gui.Animation;
 
 public interface DataLoader {
-	
+
 	public List<Animation> loadAnimationsForRobots();
+
 	public List<Animation> loadAnimationsForRobotActions();
-	
+
 	public List<Robot> loadRobots();
+
+	public List<Robot> loadUserRobots(String userId);
+
 	public List<Attack> loadRobotAttacks();
+
 	public List<Defense> loadRobotDefends();
 
 }
