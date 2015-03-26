a5f763b7231d9599efb4a4d95c0fbc83e5d557f4
Marvin Bruns <android.bruns@googlemail.com>
2015-03-26 08:53:08 +0100

    arena update

--- de.kvwl.n8dA.infrastructure.rewards.client/src/main/java/de/kvwl/n8dA/infrastructure/rewards/client/CreditAccessClient.java
+++ de.kvwl.n8dA.infrastructure.rewards.client/src/main/java/de/kvwl/n8dA/infrastructure/rewards/client/CreditAccessClient.java
@@ -20,16 +20,17 @@ import de.kvwl.n8dA.infrastructure.commons.util.NetworkUtils;
 /**
  * GUI-loser Client als Verbindung zum zentralen Punkte-Server.
  */
-public class CreditAccessClient implements CreditAccess {
+public class CreditAccessClient implements CreditAccess
+{
 
-	private static final Logger LOG = LoggerFactory
-			.getLogger(CreditAccessClient.class);
+	private static final Logger LOG = LoggerFactory.getLogger(CreditAccessClient.class);
 
 	private CreditAccesHandler server;
 	private UUID uuid;
 	private String ipAdressServer;
 
-	public CreditAccessClient(String ipAdressServer) {
+	public CreditAccessClient(String ipAdressServer)
+	{
 
 		// TODO Timo: Bau das mal auf Startparameter um
 		/*
@@ -65,10 +66,9 @@ public class CreditAccessClient implements CreditAccess {
 	 * @throws NotBoundException
 	 * @throws MalformedURLException
 	 */
-	public void initConnectionToServer() throws RemoteException,
-			MalformedURLException, NotBoundException {
-		String url = "//" + ipAdressServer + "/"
-				+ NetworkUtils.REWARD_SERVER_NAME;
+	public void initConnectionToServer() throws RemoteException, MalformedURLException, NotBoundException
+	{
+		String url = "//" + ipAdressServer + "/" + NetworkUtils.REWARD_SERVER_NAME;
 		server = (CreditAccesHandler) Naming.lookup(url);
 		LOG.info("Client: " + uuid + " connected to Server");
 	}
@@ -76,34 +76,39 @@ public class CreditAccessClient implements CreditAccess {
 	/**
 	 * Ruft den Punktestand f�r den �bergebenen Namen vom Server ab
 	 */
-	public int getConfigurationPointsForPerson(String name)
-			throws NoSuchPersonException, RemoteException {
+	public int getConfigurationPointsForPerson(String name) throws NoSuchPersonException, RemoteException
+	{
 		return server.getConfigurationPointsForPerson(name);
 	}
 
 	// TODO: Timo: Nur zu Testzwecken. Sp�ter entfernen.
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception
+	{
 
 		CreditAccessClient client = new CreditAccessClient("localhost");
-		try {
+		try
+		{
 			client.initConnectionToServer();
 
 			client.persistConfigurationPointsForPerson("Derp", "TestGame", 5);
 			client.persistConfigurationPointsForPerson("Derp", "TestGame", 6);
 			System.out.println(client.getAllGamesForPersonName("Derp"));
-		} catch (RemoteException e) {
+		}
+		catch (RemoteException e)
+		{
 			LOG.error("Remote Error", e);
 		}
 	}
 
 	@Override
-	public void persistConfigurationPointsForPerson(String personName,
-			String gameName, int points) throws RemoteException {
+	public void persistConfigurationPointsForPerson(String personName, String gameName, int points)
+		throws RemoteException
+	{
 		server.persistConfigurationPointsForPerson(personName, gameName, points);
 	}
 
-	public List<GamePerson> getAllGamesForPersonName(String personName)
-			throws RemoteException {
+	public List<GamePerson> getAllGamesForPersonName(String personName) throws RemoteException
+	{
 		return server.getAllGamesForPersonName(personName);
 	}
 
