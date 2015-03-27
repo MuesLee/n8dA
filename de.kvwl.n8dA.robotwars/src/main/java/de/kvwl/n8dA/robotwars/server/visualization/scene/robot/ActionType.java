a115cb117d6c676b427ba573cc32a3fca4ed4ec3
Marvin Bruns <android.bruns@googlemail.com>
2015-03-27 11:34:08 +0100

    docs

--- de.kvwl.n8dA.robotwars/src/main/java/de/kvwl/n8dA/robotwars/server/visualization/scene/robot/ActionType.java
+++ de.kvwl.n8dA.robotwars/src/main/java/de/kvwl/n8dA/robotwars/server/visualization/scene/robot/ActionType.java
@@ -1,5 +1,11 @@
 package de.kvwl.n8dA.robotwars.server.visualization.scene.robot;
 
+/**
+ * 
+ * Verhalten der Aktionen. Die Verteidigungen sind unterteilt in die einfache Verteidigung(totaler
+ * Block), Verteidigung mit Eigenschaden() Verteidigung mit Reflektion(kein Eigenschaden)
+ *
+ */
 public enum ActionType {
 
 	Attack, Defense, DefenseWithDamage, ReflectingDefense;
