5fd883bbe44b1b26e49c56d68e4b7941633b0c6b
Marvin Bruns <android.bruns@googlemail.com>
2015-04-02 10:13:20 +0200

    todos

--- de.kvwl.n8dA.robotwars.commons/src/main/java/de/kvwl/n8dA/robotwars/commons/game/statuseffects/TypeResistance.java
+++ de.kvwl.n8dA.robotwars.commons/src/main/java/de/kvwl/n8dA/robotwars/commons/game/statuseffects/TypeResistance.java
@@ -2,27 +2,32 @@ package de.kvwl.n8dA.robotwars.commons.game.statuseffects;
 
 import de.kvwl.n8dA.robotwars.commons.game.entities.Robot;
 
-public abstract class TypeResistance extends StatusEffect  {
+public abstract class TypeResistance extends StatusEffect
+{
 
 	private static final long serialVersionUID = 1L;
 
-	public TypeResistance(int startDuration, int roundsLeft) {
+	public TypeResistance(int startDuration, int roundsLeft)
+	{
 		super(startDuration, roundsLeft);
 	}
-	
-	public TypeResistance() {
-	}
-		
-	@Override
-	public void performInitialRobotModification(Robot robot) {
-		// TODO Auto-generated method stub
-		
+
+	public TypeResistance()
+	{
 	}
 
 	@Override
-	public void performEachRoundsModification(Robot robot) {
-		// TODO Auto-generated method stub
-		
+	public void performInitialRobotModification(Robot robot)
+	{
+		// TODO TypeResistance:performInitialRobotModification
+
+	}
+
+	@Override
+	public void performEachRoundsModification(Robot robot)
+	{
+		// TODO TypeResistance:performEachRoundsModification
+
 	}
 
 }
