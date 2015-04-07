a91b69db4b202e79b9fc8bae3123b216ffc050b4
Marvin Bruns <android.bruns@googlemail.com>
2015-04-07 12:59:44 +0200

    effects painting

--- de.kvwl.n8dA.robotwars/src/main/java/de/kvwl/n8dA/robotwars/server/visualization/scene/status/EffectLabel.java
+++ de.kvwl.n8dA.robotwars/src/main/java/de/kvwl/n8dA/robotwars/server/visualization/scene/status/EffectLabel.java
@@ -1,17 +1,21 @@
 package de.kvwl.n8dA.robotwars.server.visualization.scene.status;
 
-import java.awt.Color;
+import game.engine.image.InternalImage;
+import game.engine.stage.scene.object.SceneObject;
+
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.util.HashMap;
 import java.util.Map;
 
 import de.kvwl.n8dA.robotwars.commons.game.actions.RobotActionType;
 import de.kvwl.n8dA.robotwars.commons.game.statuseffects.StatusEffect;
-import game.engine.stage.scene.object.SceneObject;
 
 //TODO Marvin: Effect Anzeige
 public class EffectLabel extends SceneObject
 {
+
+	private static final String IMAGE_PATH = "/de/kvwl/n8dA/robotwars/commons/images/";
 
 	private Map<RobotActionType, StatusEffect> effects = new HashMap<RobotActionType, StatusEffect>();
 
@@ -19,8 +23,45 @@ public class EffectLabel extends SceneObject
 	protected void paint(Graphics2D g2d, long time)
 	{
 
-		g2d.setColor(Color.MAGENTA);
-		g2d.fillRect(0, 0, getWidth(), getHeight());
+		double _width = getWidth() * (1.0 / 3.0);
+		double _height = getHeight();
+
+		_height = _width = Math.min(_width, _height);
+
+		int _x;
+		int _y = 0;
+
+		double _space = _width * 0.1;
+
+		if (true)
+		{
+
+			Image img = InternalImage.loadFromPath(IMAGE_PATH, "stat_Water.png");
+			_x = 0;
+
+			g2d.drawImage(img, _x, _y, (int) (_x + _width), (int) (_y + _height), 0, 0, img.getWidth(null),
+				img.getHeight(null), null);
+		}
+
+		if (true)
+		{
+
+			Image img = InternalImage.loadFromPath(IMAGE_PATH, "stat_Fire.png");
+			_x = (int) (_width + _space);
+
+			g2d.drawImage(img, _x, _y, (int) (_x + _width), (int) (_y + _height), 0, 0, img.getWidth(null),
+				img.getHeight(null), null);
+		}
+
+		if (true)
+		{
+
+			Image img = InternalImage.loadFromPath(IMAGE_PATH, "stat_Plant.png");
+			_x = (int) ((_width + _space) * 2);
+
+			g2d.drawImage(img, _x, _y, (int) (_x + _width), (int) (_y + _height), 0, 0, img.getWidth(null),
+				img.getHeight(null), null);
+		}
 	}
 
 	public void setEffect(RobotActionType type, StatusEffect effect)
@@ -31,7 +72,7 @@ public class EffectLabel extends SceneObject
 
 	public void reset()
 	{
-		// TODO Auto-generated method stub
 
+		effects.clear();
 	}
 }
