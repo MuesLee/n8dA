0f13dd63f5f7eb8f820a81db57025acfd1e548da
MuesLee <stimo@gmx.net>
2015-05-21 13:15:28 +0200

    HE comparable HEC implementation

--- de.kvwl.n8dA.infrastructure.commons/src/main/java/de/kvwl/n8dA/infrastructure/commons/entity/HighscoreEntry.java
+++ de.kvwl.n8dA.infrastructure.commons/src/main/java/de/kvwl/n8dA/infrastructure/commons/entity/HighscoreEntry.java
@@ -1,6 +1,6 @@
 package de.kvwl.n8dA.infrastructure.commons.entity;
 
-public class HighscoreEntry {
+public class HighscoreEntry implements Comparable<HighscoreEntry>{
 	
 	String name;
 	int points;
@@ -30,6 +30,51 @@ public class HighscoreEntry {
 	public void setHighlighted(boolean highlighted) {
 		this.highlighted = highlighted;
 	}
+	@Override
+	public int compareTo(HighscoreEntry o) {
+		
+		if(o.equals(this))
+		{
+			return 0;
+		}
+		else if(o.getPoints()>this.points)
+		{
+			return 1;
+		}
+		else {
+			return -1;
+		}
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (highlighted ? 1231 : 1237);
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + points;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		HighscoreEntry other = (HighscoreEntry) obj;
+		if (highlighted != other.highlighted)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (points != other.points)
+			return false;
+		return true;
+	}
+	
 	
 	
 
--- de.kvwl.n8dA.infrastructure.rewardserver/src/main/java/de/kvwl/n8dA/infrastructure/controller/HighscoreController.java
+++ de.kvwl.n8dA.infrastructure.rewardserver/src/main/java/de/kvwl/n8dA/infrastructure/controller/HighscoreController.java
@@ -1,58 +1,65 @@
 package de.kvwl.n8dA.infrastructure.controller;
 
+import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.kvwl.n8dA.infrastructure.commons.entity.Game;
+import de.kvwl.n8dA.infrastructure.commons.entity.GamePerson;
 import de.kvwl.n8dA.infrastructure.commons.entity.HighscoreEntry;
 import de.kvwl.n8dA.infrastructure.commons.interfaces.BasicCreditAccess;
 import de.kvwl.n8dA.infrastructure.commons.interfaces.HighscoreListVisualizer;
 import de.kvwl.n8dA.infrastructure.rewards.gui.HighscoreListVisualizerImpl;
 
-public class HighscoreController
-{
+public class HighscoreController {
+	private static final Logger LOG = LoggerFactory
+			.getLogger(HighscoreController.class);
 
 	private static final int _LIST_SWITCH_RATE_IN_MS = 10000;
 
 	private static int CURRENT_LIST_INDEX = 0;
 
-	private List<HighscoreEntry> highscoreEntries;
+	private List<Game> games;
 
 	Timer timer;
 	BasicCreditAccess server;
 	HighscoreListVisualizer visualizer;
 
-	public HighscoreController(BasicCreditAccess server)
-	{
+	public HighscoreController(BasicCreditAccess server) {
 		this.server = server;
 		init();
 
 	}
 
-	private void init()
-	{
-		highscoreEntries = new ArrayList<>();
+	private void init() {
+		games = new ArrayList<>();
 		timer = new Timer("HighscoreListSwitch-Timer");
+
+		refreshGameList();
 
 		HighscoreListVisualizerImpl visualizer = new HighscoreListVisualizerImpl();
 		visualizer.setVisible(true);
 		this.visualizer = visualizer;
 	}
 
-	public void showHighscoreView()
-	{
+	public void showHighscoreView() {
 		startTimer();
 	}
 
-	private void startTimer()
-	{
-		TimerTask task = new TimerTask()
-		{
+	private void startTimer() {
+		TimerTask task = new TimerTask() {
 
 			@Override
-			public void run()
-			{
+			public void run() {
 				switchHighscoreList();
 			}
 
@@ -61,9 +68,62 @@ public class HighscoreController
 		timer.schedule(task, 0, _LIST_SWITCH_RATE_IN_MS);
 	}
 
-	private void switchHighscoreList()
-	{
+	void switchHighscoreList() {
+		Game game = games.get(CURRENT_LIST_INDEX);
+		String gameName = game.getName();
 
+		try {
+			server.getAllGamePersonsForGame(gameName);
+		} catch (RemoteException e) {
+			LOG.error("GamePersonList für " + gameName
+					+ " konnte nicht vom Server abgerufen werden", e);
+		}
 	}
 
+	private void refreshGameList() {
+		try {
+			games = server.getAllGames();
+		} catch (RemoteException e) {
+			LOG.error("GameList konnte vom Server nicht abgerufen werden", e);
+		}
+	}
+
+	List<HighscoreEntry> createHighscoreList(
+			List<GamePerson> gamePersons) {
+		List<HighscoreEntry> highscoreEntries = new ArrayList<>();
+
+		Map<String, Integer> aggregatedPoints = aggregateGamePersonsPoints(gamePersons);
+		
+		Set<String> persons = aggregatedPoints.keySet();
+		for (String person : persons) {
+			
+			int points = aggregatedPoints.get(person);
+			HighscoreEntry highscoreEntry = new HighscoreEntry(person, points);
+			highscoreEntries.add(highscoreEntry);
+		}
+
+		Collections.sort(highscoreEntries);
+		
+		return highscoreEntries;
+	}
+	
+	private Map<String, Integer> aggregateGamePersonsPoints(List<GamePerson> gamePersons)
+	{
+		Map<String, Integer> result = new HashMap<>();
+		
+		for (GamePerson gamePerson : gamePersons) {
+			String personName = gamePerson.getPerson().getName();
+			Integer gamePoints = gamePerson.getPoints();
+			
+			if(result.containsKey(personName))
+			{
+				Integer storedGamePoints = result.get(personName);
+				gamePoints += storedGamePoints;
+			}
+			
+			result.put(personName, gamePoints);
+		}
+		
+		return result;
+	}
 }
